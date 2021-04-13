1e6a733709a83c0db9efa7ce71f167c2705343c0
Muhammad Asadullah <muhammad.asad2020@gmail.com>
2021-04-13 12:45:49 -0700

    useState implementation

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [todos, setTodos] = useState([
+    "Send an email update to the team: 9am today",
+    "Call the design agency to finalize mockups: 1:00pm today",
+    "Touch base with recruiters about new role: Tuesday",
+    "Meet with the engineering team: Thursday",
+  ]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello asadcs</h1>
+      <input />
+      <button>Add Todo</button>
+      <ul>
+        {todos.map((todo) => (
+          <li>{todo}</li>
+        ))}
+      </ul>
     </div>
   );
 }
